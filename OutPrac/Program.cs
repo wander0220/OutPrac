84b5228e2e09273f916bc359a27a090599fdbc84
wander0220 <s2018s26@e-mirim.hs.kr>
2020-06-30 09:33:45 +0900

    초기 커밋.

---
+++ OutPrac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자 입력 : ");
+            int output;
+            bool result = int.TryParse(
+                Console.ReadLine(), out output);
+
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자 : " + output);
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력해 주세요!");
+            }
+        }
+    }
+}
